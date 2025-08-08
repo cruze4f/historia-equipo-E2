a6c2fdaed7feeb2436561d1e24f2d094f3d914e6
Heriberto <riveraromero60@gmail.com>
2025-08-07 22:58:40 -0600

    parte 3 de la historia real

--- historia.py
+++ historia.py
@@ -1,5 +1,5 @@
 print("Parte 1: Había una vez un grupo muy bueno de musica en mexico..")
 
 
-print("parte 3: al final el dragon resulto ser la mascota perdida")#integrante 3
+print("y terminaron en bolsas")#integrante 3
 
